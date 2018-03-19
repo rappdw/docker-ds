456c5a9909aa687f78633873d2eca833d3b072e9
Dan Rapp <drapp@proofpoint.com>
2018-03-19 10:46:41 -0600

    Fix script tagging error

--- README.md
+++ README.md
@@ -75,6 +75,7 @@ Requires credstash credential named: google.drive.client_id
 
 ## Versions
 
+1.0.15 - start-project-notebook.sh more robust
 1.0.14 - Add labmanager and matplotlib jupyterlab extensions
 1.0.13 - Include GraphViz in the image
 1.0.12 - Update to latest CUDA cdNN Tensorflow
--- docker/notebook/start-project-notebook.sh
+++ docker/notebook/start-project-notebook.sh
@@ -5,14 +5,20 @@
 CPU_GPU_ENV=${CPU_GPU_ENV:-"/cpu-env"}
 . $CPU_GPU_ENV
 
-# install an editable version of the ner module into site-packages
-pip install -e /home/jovyan/project/
+if [ -e /home/jovyan/project/setup.py ]
+then
+    # install an editable version of the ner module into site-packages
+    pip install -e /home/jovyan/project/
+fi
 
-# for any notebooks in the 'notebooks' sub-dir, sign them, we will "trust" them
-pushd /home/jovyan/project/notebooks
-for f in *.ipynb; do
-    jupyter trust "$f"
-done
-popd
+if [ -e /home/jovyan/project/notebooks ]
+then
+    # for any notebooks in the 'notebooks' sub-dir, sign them, we will "trust" them
+    pushd /home/jovyan/project/notebooks
+    for f in *.ipynb; do
+        jupyter trust "$f"
+    done
+    popd
+fi
 
 exec start-notebook.sh
