ecd1296560f117f728abcb0e5719e6dceab61318
Dan Rapp <rappdw@gmail.com>
2018-04-04 07:56:30 -0600

    Add lantern Improve build (install multiple extensions) Remove github and google-drive extensions

--- root/usr/local/bin/setup-labs.sh
+++ root/usr/local/bin/setup-labs.sh
@@ -2,13 +2,11 @@
 
 echo "**** enable jupyterlab ****"
 jupyter serverextension enable --py jupyterlab
-echo "**** install github extension ****"
-jupyter labextension install @jupyterlab/github
-echo "**** enable github extension ****"
-jupyter serverextension enable --sys-prefix --py jupyterlab_github
-echo "**** enable bokeh extension ****"
-jupyter labextension install jupyterlab_bokeh
-echo "**** setup collaboration via Google Drive ****"
-jupyter labextension install @jupyterlab/google-drive
-echo "**** setup interaactive matplotlib ****"
-jupyter labextension install @jupyter-widgets/jupyterlab-manager jupyter-matplotlib
+#echo "**** enable github extension ****"
+#jupyter serverextension enable --sys-prefix --py jupyterlab_github
+echo "**** setup lab extensions (matplotlib, bokeh, plotly, etc.) ****"
+jupyter labextension install @jupyter-widgets/jupyterlab-manager jupyter-matplotlib jupyterlab_bokeh @pyviz/jupyterlab_holoviews @jupyterlab/plotly-extension qgrid @jpmorganchase/perspective-jupyterlab pylantern
+#echo "**** install github extension ****"
+#jupyter labextension install @jupyterlab/github
+#echo "**** setup collaboration via Google Drive ****"
+#jupyter labextension install @jupyterlab/google-drive
