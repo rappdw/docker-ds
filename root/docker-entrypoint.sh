cc3bb38b934fc392b8bbee6a17545a406423e5dd
Dan Rapp <drapp@proofpoint.com>
2019-05-13 13:02:53 -0600

    Fix image nmage (resero-labs should be resero) Handle cases where requirements.txt is present, but setup.py is not.

--- root/docker-entrypoint.sh
+++ root/docker-entrypoint.sh
@@ -6,10 +6,16 @@ WORKDIR=${WORKDIR:-"/workdir"}
 # install the module mounted in $WORKDIR
 if [ -e $WORKDIR/setup.py ]; then
     pip install -e $WORKDIR
+elif [ -e $WORKDIR/requirements.txt ]; then
+    pip install -r $WORKDIR/requirements.txt
 elif [ -e /home/jovyan/setup.py ]; then
     pip install -e /home/jovyan
+elif [ -e /home/jovyan/requirements.txt ]; then
+    pip install -r /home/jovyan/requirements.txt
 elif [ -e /home/jovyan/project/setup.py ]; then
     pip install -e /home/jovyan/project
+elif [ -e /home/jovyan/project/requirements.txt ]; then
+    pip install -r /home/jovyan/project/requirements.txt
 fi
 
 HOME=/home/jovyan
